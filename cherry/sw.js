075a4f29222c3812e8477a58d46aa757eed474b4
hiro-nihongo <shigakith@gmail.com>
2026-09-14 19:57:49 +0700

    改訂２

--- cherry/sw.js
+++ cherry/sw.js
@@ -4,7 +4,7 @@
 
    ▼ 中身を直したら、必ず下の数字を1つ上げる
 ------------------------------------------------ */
-const VERSION = 21;
+const VERSION = 26;
 
 const CORE_CACHE  = `cherry-core-v${VERSION}`;
 const AUDIO_CACHE = "cherry-audio-v6";
